df480acae68a18530b04a2743a7d917329530299
Ivan Sevcik <iv.sevcik@gmail.com>
2026-01-27 18:18:24 +0100

    feat(webapi): remove weatherforecast example endpoint

--- EmployeeManager.WebAPI/Program.cs
+++ EmployeeManager.WebAPI/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddControllers();
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddAutoMapper(typeof(MappingProfile));
@@ -40,7 +39,7 @@ if (args.Contains("--seed-employees"))
     }
 }
 
-// Configure the HTTP request pipeline.
+// Swagger configuration
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -52,31 +51,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.MapControllers();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
