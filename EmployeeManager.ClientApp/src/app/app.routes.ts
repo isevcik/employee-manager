96346bfc9b109751ceddcc7f813280e6b350d48d
Ivan Sevcik <iv.sevcik@gmail.com>
2026-01-27 20:42:45 +0100

    feat(clientapp): add ng-zorro ui kit

--- EmployeeManager.ClientApp/src/app/app.routes.ts
+++ EmployeeManager.ClientApp/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { EmployeesComponent } from './employees/employees';
-import { CountriesComponent } from './countries/countries';
-import { JobCategoriesComponent } from './job-categories/job-categories';
+import { CountriesComponent } from './pages/countries/countries';
+import { EmployeesComponent } from './pages/employees/employees';
+import { JobCategoriesComponent } from './pages/job-categories/job-categories';
 
 export const routes: Routes = [
   { path: 'employees', component: EmployeesComponent },
   { path: 'countries', component: CountriesComponent },
-  { path: 'job-categories', component: JobCategoriesComponent }
+  { path: 'job-categories', component: JobCategoriesComponent },
+
+  { path: '', redirectTo: '/employees', pathMatch: 'full' },
 ];
