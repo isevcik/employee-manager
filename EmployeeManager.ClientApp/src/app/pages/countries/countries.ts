257d518e13f43bad770fbd05c3156839d040cb03
Ivan Sevcik <iv.sevcik@gmail.com>
2026-01-27 21:53:30 +0100

    feat(clientapp): implement basic table pages for employees, countries and job categories

--- EmployeeManager.ClientApp/src/app/pages/countries/countries.ts
+++ EmployeeManager.ClientApp/src/app/pages/countries/countries.ts
@@ -1,10 +1,19 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { CountriesService, CountryDto } from '../../api';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.html',
   styleUrl: './countries.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [AsyncPipe, NzTableModule],
 })
 export class CountriesComponent {
+  private countriesService = inject(CountriesService);
+
+  countries$: Observable<CountryDto[]> = this.countriesService.apiCountriesGet();
 }
--- EmployeeManager.ClientApp/src/app/pages/employees/employees.ts
+++ EmployeeManager.ClientApp/src/app/pages/employees/employees.ts
@@ -1,10 +1,18 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { EmployeesService, EmployeeGetDto } from '../../api';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.html',
   styleUrl: './employees.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [AsyncPipe, NzTableModule],
 })
 export class EmployeesComponent {
+  private employeesService = inject(EmployeesService);
+
+  employees$: Observable<EmployeeGetDto[]> = this.employeesService.apiEmployeesGet();
 }
