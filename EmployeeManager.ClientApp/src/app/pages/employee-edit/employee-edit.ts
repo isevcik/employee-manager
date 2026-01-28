7cae2e5ea93766ae1dc30224e3b408696d1941e5
Ivan Sevcik <iv.sevcik@gmail.com>
2026-01-28 01:43:37 +0100

    feat(clientapp): implement employee edit page: use date picker for date inputs

--- EmployeeManager.ClientApp/src/app/pages/employee-edit/employee-edit.ts
+++ EmployeeManager.ClientApp/src/app/pages/employee-edit/employee-edit.ts
@@ -7,6 +7,7 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzRadioModule } from 'ng-zorro-antd/radio';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
@@ -15,7 +16,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './employee-edit.html',
   styleUrl: './employee-edit.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [ReactiveFormsModule, NzFormModule, NzInputModule, NzButtonModule, NzRadioModule, NzSelectModule, NzDividerModule, CommonModule],
+  imports: [ReactiveFormsModule, NzFormModule, NzInputModule, NzButtonModule, NzRadioModule, NzSelectModule, NzDividerModule, NzDatePickerModule, CommonModule],
 })
 export class EmployeeEditComponent {
   private employeesService = inject(EmployeesService);
@@ -34,7 +35,7 @@ export class EmployeeEditComponent {
     lastName: ['', [Validators.required]],
     email: ['', [Validators.required, Validators.email]],
     phoneNumber: [''],
-    birthDate: ['', [Validators.required]],
+    birthDate: [null as Date | null, [Validators.required]],
     gender: ['', [Validators.required]],
     jobCategories: [[] as number[]],
     salary: [null as number | null],
@@ -43,8 +44,8 @@ export class EmployeeEditComponent {
     zipCode: [''],
     city: [''],
     countryId: [null as number | null],
-    joinedDate: ['', [Validators.required]],
-    exitedDate: [''],
+    joinedDate: [null as Date | null, [Validators.required]],
+    exitedDate: [null as Date | null],
   });
 
   constructor() {
@@ -56,7 +57,7 @@ export class EmployeeEditComponent {
           lastName: employee.lastName || '',
           email: employee.email || '',
           phoneNumber: employee.phoneNumber || '',
-          birthDate: employee.birthDate || '',
+          birthDate: employee.birthDate ? new Date(employee.birthDate) : null,
           gender: employee.gender || '',
           jobCategories: employee.jobCategories?.map(jc => jc.id).filter((id): id is number => id !== undefined) || [],
           salary: employee.salary?.amount as number | null || null,
@@ -65,8 +66,8 @@ export class EmployeeEditComponent {
           zipCode: employee.address?.zipCode || '',
           city: employee.address?.city || '',
           countryId: employee.address?.countryId as number | null || null,
-          joinedDate: employee.joinedDate || '',
-          exitedDate: employee.exitedDate || '',
+          joinedDate: employee.joinedDate ? new Date(employee.joinedDate) : null,
+          exitedDate: employee.exitedDate ? new Date(employee.exitedDate) : null,
         });
       });
     });
